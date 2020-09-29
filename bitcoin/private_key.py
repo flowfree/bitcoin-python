7ee335375c57efe6c32f61f60fc23689d57800af
Nash <nashruddin.amin@gmail.com>
2020-09-29 11:50:15 +0700

    Added PrivateKey class

---
+++ bitcoin/private_key.py
@@ -0,0 +1,22 @@
+from random import randint 
+
+from . import N, G
+from .signature import Signature
+
+
+class PrivateKey(object):
+    def __init__(self, secret):
+        self.secret = secret
+        self.point = secret * G
+
+    def __str__(self):
+        return '{:x}'.format(self.secret).zfill(64)
+
+    def sign(self, z):
+        k = randint(0, N)
+        r = (k * G).x.num
+        k_inv = pow(k, N-2, N)
+        s = (z + r * self.secret) * k_inv % N
+        if s > N/2:
+            s = N - s
+        return Signature(r, s)
