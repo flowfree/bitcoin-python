6d6d819d7a6790802e67cda06b664269a352a65f
Nash <nashruddin.amin@gmail.com>
2020-10-04 10:44:20 +0700

    Parse Script from stream

--- bitcoin/script.py
+++ bitcoin/script.py
@@ -1,9 +1,42 @@
-from .helpers import read_varints
+from .helpers import read_varints, little_endian_to_int
 
 
 class Script(object):
+    def __init__(self, cmds=None):
+        if cmds is None:
+            self.cmds = []
+        else:
+            self.cmds = cmds
+
     @staticmethod
     def parse(stream):
+        """
+        Read and parse Script from a stream.
+        """
+
         length = read_varints(stream)
-        _ = stream.read(length)
-        return Script()
+        cmds = []
+        count = 0
+        while count < length:
+            current = stream.read(1)
+            count += 1
+            current_byte = current[0]
+            if current_byte >= 1 and current_byte <= 75:
+                n = current_byte
+                cmds.append(stream.read(n))
+                count += n
+            elif current_byte == 76:
+                data_length = little_endian_to_int(stream.read(1))
+                cmds.append(stream.read(data_length))
+                count += data_length + 1
+            elif current_byte == 77:
+                data_length = little_endian_to_int(stream.read(2))
+                cmds.append(stream.read(data_length))
+                count += data_length + 2
+            else:
+                op_code = current_byte
+                cmds.append(op_code)
+        
+        if count != length:
+            raise SyntaxError('Parsing script failed.')
+        return Script(cmds)
